8749cec695f410a88b94e5ff3d7ce0a326684295
Jing <jing.liu@mapbox.com>
2018-08-24 11:47:21 +0800

    Update measure.js

--- docs/pages/example/measure.js
+++ docs/pages/example/measure.js
@@ -1,7 +1,7 @@
 /*---
 title: 测距
 description: >-
-  单击地图上的点以使用[turf.lineDistance](http://turfjs.org/static/docs/module-turf_line-distance.html)创建测距的线段。
+  单击地图上的点以使用 [turf.lineDistance](http://turfjs.org/static/docs/module-turf_line-distance.html)创建测距的线段。
 tags:
   - user-interaction
 pathname: /mapbox-gl-js/example/measure/
