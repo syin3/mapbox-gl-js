b8e32101663ef27d4013981b9be7434449b1c7df
syin3 <42665592+syin3@users.noreply.github.com>
2018-08-25 00:09:22 -0700

    Translate down 100%
    Line 2 to Line 6

--- docs/pages/example/geojson-layer-in-stack.js
+++ docs/pages/example/geojson-layer-in-stack.js
@@ -1,9 +1,9 @@
 /*---
-title: Add a new layer below labels
-description: 'Using the second argument of [`addLayer`](/mapbox-gl-js/api/#map#addlayer), you can be more precise'
-tags:
-  - layers
-pathname: /mapbox-gl-js/example/geojson-layer-in-stack/
+标题: 在标签下增添一个新图层
+描述: '通过[`addLayer`](/mapbox-gl-js/api/#map#addlayer)的第二个参数可以更精确地描述'
+标签:
+  - 图层
+路径名称: /mapbox-gl-js/example/geojson-layer-in-stack/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-layer-in-stack.html';
