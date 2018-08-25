44d01ee384b0ccacc557f32be2a12c345f9f2d78
syin3 <42665592+syin3@users.noreply.github.com>
2018-08-24 23:25:15 -0700

    Translate down 100%
    Line 2 to Line 6

--- docs/pages/example/geojson-markers.js
+++ docs/pages/example/geojson-markers.js
@@ -1,9 +1,9 @@
 /*---
-title: Draw GeoJSON points
-description: Draw points from a GeoJSON collection to a map.
-tags:
-  - layers
-pathname: /mapbox-gl-js/example/geojson-markers/
+标题: 绘制GeoJSON中的点
+描述: 将GeoJSON集中的点绘制到地图上
+标签:
+  - 图层
+路径: /mapbox-gl-js/example/geojson-markers/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-markers.html';
