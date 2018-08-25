de89f4d9197d5e4639843129fef3b77d037b304d
syin3 <42665592+syin3@users.noreply.github.com>
2018-08-24 23:28:00 -0700

    Translate down 100%
    Line 2 to Line 6

--- docs/pages/example/heatmap-layer.js
+++ docs/pages/example/heatmap-layer.js
@@ -1,9 +1,9 @@
 /*---
-title: Create a heatmap layer
-description: Visualize earthquake frequency by location using a heatmap layer.
-tags:
-  - layers
-pathname: /mapbox-gl-js/example/heatmap-layer/
+标题: 生成一个热力图图层
+描述: 通过一个热力图图层展示不同地点的地震频率
+标签:
+  - 图层
+路径: /mapbox-gl-js/example/heatmap-layer/
 ---*/
 import Example from '../../components/example';
 import html from './heatmap-layer.html';
