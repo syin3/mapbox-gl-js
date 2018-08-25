ac7a71d0857011729cd1b4b07d9b7755adac229d
syin3 <42665592+syin3@users.noreply.github.com>
2018-08-24 22:06:10 -0700

    Translate down 100%
    Line 2 to Line 6

--- docs/pages/example/data-driven-lines.js
+++ docs/pages/example/data-driven-lines.js
@@ -1,9 +1,9 @@
 /*---
-title: Style lines with a data-driven property
-description: Creating a visualization with a data expression for line-color
-tags:
-  - layers
-pathname: /mapbox-gl-js/example/data-driven-lines/
+标题: 使用数据性质为图线设计可视化效果
+描述: 使用数据语句为图线颜色定义可视化效果Creating a visualization with a data expression for line-color
+标签:
+  - 图层
+路径: /mapbox-gl-js/example/data-driven-lines/
 ---*/
 import Example from '../../components/example';
 import html from './data-driven-lines.html';
