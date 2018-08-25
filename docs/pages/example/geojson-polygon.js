b24aca3dd0218a4ec800b1f5f17acb2f8a091b6f
syin3 <42665592+syin3@users.noreply.github.com>
2018-08-24 23:30:55 -0700

    Translate down 100%
    Line 2 to Line 6

--- docs/pages/example/geojson-polygon.js
+++ docs/pages/example/geojson-polygon.js
@@ -1,9 +1,9 @@
 /*---
-title: Add a GeoJSON polygon
-description: Style a polygon with the fill layer type
-tags:
-  - layers
-pathname: /mapbox-gl-js/example/geojson-polygon/
+标题: 增添一个GeoJSON多边形
+描述: 使用填充图层类型指定当前新多边形
+标签:
+  - 图层
+路径: /mapbox-gl-js/example/geojson-polygon/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-polygon.html';
