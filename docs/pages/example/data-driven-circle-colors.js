ce31e16ad60bc50229e079dcc14c48c9f5cd12fc
syin3 <42665592+syin3@users.noreply.github.com>
2018-08-25 00:03:30 -0700

    Translate down 100%
    Line 2 to Line 6

--- docs/pages/example/data-driven-circle-colors.js
+++ docs/pages/example/data-driven-circle-colors.js
@@ -1,9 +1,9 @@
 /*---
-title: Style circles with a data-driven property
-description: Creating a visualization with a data expression for [`circle-color`](/mapbox-gl-js/style-spec#paint-circle-circle-color)
-tags:
-  - layers
-pathname: /mapbox-gl-js/example/data-driven-circle-colors/
+标题: 使用数据值为圆点定制特征
+描述: 通过数据表达式为[`circle-color`](/mapbox-gl-js/style-spec#paint-circle-circle-color)定制可视化效果
+标签:
+  - 图层
+路径名称: /mapbox-gl-js/example/data-driven-circle-colors/
 ---*/
 import Example from '../../components/example';
 import html from './data-driven-circle-colors.html';
