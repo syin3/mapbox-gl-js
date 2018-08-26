29dfdaecfc684591c52b0ba32f6fb6f2a93857df
Jing <jing.liu@mapbox.com>
2018-08-26 10:48:37 +0800

    Update data-driven-lines.js

--- docs/pages/example/data-driven-lines.js
+++ docs/pages/example/data-driven-lines.js
@@ -1,9 +1,9 @@
 /*---
-标题: 通过数据值为图线定制可视化效果
-描述: 通过数据语句为[`line-color`](/mapbox-gl-js/style-spec#paint-line-line-color)定制可视化效果
-标签:
-  - 图层
-路径名称: /mapbox-gl-js/example/data-driven-lines/
+title: 通过数据值为图线定制可视化效果
+description: 通过数据语句为[`line-color`](/mapbox-gl-js/style-spec#paint-line-line-color)定制可视化效果
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/data-driven-lines/
 ---*/
 import Example from '../../components/example';
 import html from './data-driven-lines.html';
