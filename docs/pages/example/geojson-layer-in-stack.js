b9e4c6093c25df9688f7f90746437c7ca60669ed
Jing <jing.liu@mapbox.com>
2018-08-26 10:55:44 +0800

    Update geojson-layer-in-stack.js

--- docs/pages/example/geojson-layer-in-stack.js
+++ docs/pages/example/geojson-layer-in-stack.js
@@ -1,9 +1,9 @@
 /*---
-标题: 在标签下增添一个新图层
-描述: '通过[`addLayer`](/mapbox-gl-js/api/#map#addlayer)的第二个参数可以更精确地描述'
-标签:
-  - 图层
-路径名称: /mapbox-gl-js/example/geojson-layer-in-stack/
+title: 在标签下增添一个新图层
+description: '通过[`addLayer`](/mapbox-gl-js/api/#map#addlayer)的第二个参数可以更精确地描述'
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/geojson-layer-in-stack/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-layer-in-stack.html';
