5127befe760324c028bd4fdff178ad1b1658ec73
Jing <jing.liu@mapbox.com>
2018-08-26 10:58:30 +0800

    Update heatmap-layer.js

--- docs/pages/example/heatmap-layer.js
+++ docs/pages/example/heatmap-layer.js
@@ -1,9 +1,9 @@
 /*---
-标题: 创建一个热力图图层
-描述: 通过一个[heatmap layer](/mapbox-gl-js/style-spec#layers-heatmap)展示不同地点的地震频率.
-标签:
-  - 图层
-路径名称: /mapbox-gl-js/example/heatmap-layer/
+title: 创建一个热力图图层
+description: 通过一个[heatmap layer](/mapbox-gl-js/style-spec#layers-heatmap)展示不同地点的地震频率.
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/heatmap-layer/
 ---*/
 import Example from '../../components/example';
 import html from './heatmap-layer.html';
