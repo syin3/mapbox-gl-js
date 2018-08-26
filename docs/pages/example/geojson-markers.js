836556dbb36c5b8ee38e8b13f9fcd5ff9386663a
Jing <jing.liu@mapbox.com>
2018-08-26 10:57:05 +0800

    Update geojson-markers.js

--- docs/pages/example/geojson-markers.js
+++ docs/pages/example/geojson-markers.js
@@ -1,9 +1,9 @@
 /*---
-标题: 绘制GeoJSON点
-描述: 将一个GeoJSON集合中的点绘制到地图上.
-标签:
-  - 图层
-路径名称: /mapbox-gl-js/example/geojson-markers/
+title: 绘制GeoJSON点
+description: 将一个GeoJSON集合中的点绘制到地图上.
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/geojson-markers/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-markers.html';
