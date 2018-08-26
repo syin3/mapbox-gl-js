1b92302c52395d30a4b0fe48625675a691c1c388
Jing <jing.liu@mapbox.com>
2018-08-26 10:59:24 +0800

    Update geojson-polygon.js

--- docs/pages/example/geojson-polygon.js
+++ docs/pages/example/geojson-polygon.js
@@ -1,9 +1,9 @@
 /*---
-标题: 添加一个GeoJSON多边形
-描述: 使用[fill layer](/mapbox-gl-js/style-spec#layers-fill)的类型来创建一个GeoJSON多边形
-标签:
-  - 图层
-路径名称: /mapbox-gl-js/example/geojson-polygon/
+title: 添加一个GeoJSON多边形
+description: 使用[fill layer](/mapbox-gl-js/style-spec#layers-fill)的类型来创建一个GeoJSON多边形
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/geojson-polygon/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-polygon.html';
